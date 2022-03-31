4c0cb144ca5b1b23fbaf6864a8196a203e6a771a
marcosricciardi12 <marcosricciardi12@gmail.com>
2022-03-31 12:56:14 -0300

    tp1 fork y suma de pares

--- Popen/arg-popen.py
+++ Popen/arg-popen.py
@@ -6,26 +6,31 @@ import os
 
 from click import command
 
-parser = argparse.ArgumentParser(description="sdasdas")
+def main():
 
-parser.add_argument("-c", "--command", type=str, required=True, help="string")
-parser.add_argument("-f", "--outputfile", type=str, required=True, help="string")
-parser.add_argument("-l", "--logfile", type=str, required=True, help="string")
-args = parser.parse_args()
+    parser = argparse.ArgumentParser(description="sdasdas")
 
-print('Command: %s' % args.command.split())
-print('Output File: %s' % args.outputfile)
-print('Log File: %s' % args.logfile)
+    parser.add_argument("-c", "--command", type=str, required=True, help="string")
+    parser.add_argument("-f", "--outputfile", type=str, required=True, help="string")
+    parser.add_argument("-l", "--logfile", type=str, required=True, help="string")
+    args = parser.parse_args()
 
-with open(args.outputfile, "ab") as outputfile:
-    process = sp.Popen(args.command.split(), stdout=sp.PIPE, stderr=sp.PIPE)
-    stdout, stderr = process.communicate()
-    process2 = sp.Popen("date", stdout=sp.PIPE, stderr=sp.PIPE)
-    stdout2, stderr2 = process2.communicate()
-    outputfile.write(stdout)
+    print('Command: %s' % args.command.split())
+    print('Output File: %s' % args.outputfile)
+    print('Log File: %s' % args.logfile)
 
-with open(args.logfile, "ab") as logfile:
-    if stderr == b'':
-        logfile.write(stdout2[:-2] + b': Comando ' + bytes(args.command, encoding = "utf-8") + b' ejecutado correctamente.\n')
-    else:
-        logfile.write(stdout2[:-2] + b' ' + stderr)
+    with open(args.outputfile, "ab") as outputfile:
+        process = sp.Popen(args.command.split(), stdout=sp.PIPE, stderr=sp.PIPE, shell=True)
+        stdout, stderr = process.communicate()
+        process2 = sp.Popen("date", stdout=sp.PIPE, stderr=sp.PIPE)
+        stdout2, stderr2 = process2.communicate()
+        outputfile.write(stdout)
+
+    with open(args.logfile, "ab") as logfile:
+        if stderr == b'':
+            logfile.write(stdout2[:-2] + b': Comando ' + bytes(args.command, encoding = "utf-8") + b' ejecutado correctamente.\n')
+        else:
+            logfile.write(stdout2[:-2] + b' ' + stderr)
+
+if __name__ == '__main__':
+    main()
---
+++ Process Fork/process-fork.py
@@ -0,0 +1,52 @@
+# Escribir un programa en Python que reciba los siguientes argumentos por línea de comandos:
+
+# -n <numero>
+# -h
+# -v
+# El programa debe generar <numero> procesos hijos, y cada proceso calculará la suma de todos los números enteros pares entre 0 y su número de PID.
+
+# El programa deberá mostrar por pantalla:
+
+# PID – PPID : <suma_pares>
+
+# El proceso padre debe esperar a que todos sus hijos terminen.
+# La opción -h mostrará ayuda de uso, y la opción -v habilitará el modo verboso de la aplicación. 
+# El modo verboso debe mostrar, además de la suma, un mensaje al inicio y al final de la ejecución de cada proceso hijo, 
+# que indique su inicio y fin.
+
+
+import argparse
+import sys
+import time
+import subprocess as sp
+import os
+from os import fork
+from click import command
+
+def create_and_calculate(number, verbose):
+    for i in range(number):
+        fpid = fork()
+        if fpid == 0:
+            suma = 0
+            pid = os.getpid()
+            ppid = os.getppid()
+            if verbose:
+                print("Starting process %d" % pid)
+            for k in range(0, pid+1, 2):
+                suma = k + suma
+            if verbose:
+                print("Ending process %d" % pid)
+            print("%d - %d : %d" % (pid, ppid, suma))
+            os._exit(0)
+
+def main():
+
+    parser = argparse.ArgumentParser(description="sdasdas")
+    parser.add_argument("-n", "--number", type=int, required=True, help="Ingrese el numero de procesos a crear")
+    parser.add_argument("-v", "--verbose", action='store_true', help="Ejecutar programa en modo verboso")
+    args = parser.parse_args()
+    create_and_calculate(args.number, args.verbose)
+    os.wait()
+
+if __name__ == '__main__':
+    main()
