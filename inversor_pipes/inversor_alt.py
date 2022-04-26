1e84f5260a5d385884eadd8c0672260ca18c4453
marcosricciardi12 <marcosricciardi12@gmail.com>
2022-04-25 21:07:10 -0300

    Inversor con un solo pipe

--- inversor_pipes/inversor_alt.py
+++ inversor_pipes/inversor_alt.py
@@ -26,30 +26,7 @@ import os
 from os import fork
 from click import command
 from numpy import number
-
-def invertline(line, r, w, r1, w1, i):
-    r[i], w[i] = os.pipe()
-    print("i:%d  %d -- %d" % (i, r[i], w[i]))
-    r1[i], w1[i] = os.pipe()
-    print("i:%d  %d -- %d" % (i, r1[i], w1[i]))
-    fpid = fork()
-    if fpid == 0:
-        os.close(w[i])
-        os.close(r1[i])
-        r[i] = os.fdopen(r[i])
-        linea = r[i].read()
-        w1[i] = os.fdopen(w1[i],'w')
-        w1[i].write("%s" % linea[::-1])
-        w1[i].flush()
-        w1[i].close
-        os._exit(0)
-    else:
-        os.close(r[i])
-        os.close(w1[i])
-        w[i] = os.fdopen(w[i],'w')
-        w[i].write("%s" % line)
-        w[i].flush()
-        w[i].close()
+    
 
 def main():
     parser = argparse.ArgumentParser(description="INVERSOR DE LINEAS.\n Por cada linea que tenga el archivo a leer,"
@@ -61,20 +38,34 @@ def main():
     args = parser.parse_args()
     with open(args.path, "r") as file1:
         total_lines = sum(1 for line in file1)
-    r = [[] for x in range(total_lines)]
-    w = [[] for y in range(total_lines)]
-    r1 = [[] for z in range(total_lines)]
-    w1 = [[] for w in range(total_lines)]
     file1 = open(args.path, "r")
     count = 0
-    for line in file1:
-        invertline(line, r, w, r1, w1, count)
-        count = count + 1
+    r, w = os.pipe()
+    r1, w1 = os.pipe()
     for i in range(total_lines):
-        r1[i] = os.fdopen(r1[i])
-        linea = r1[i].read()
+        fpid = fork()
+        if fpid == 0:
+            os.close(w)
+            os.close(r1)
+            r = os.fdopen(r)
+            linea = r.read()
+            w1 = os.fdopen(w1,'w')
+            w1.write("%s" % linea[::-1])
+            w1.flush()
+            w1.close()
+            os._exit(0)
+
+    w = os.fdopen(w, 'w')
+    for lines in file1:
+        w.write(lines)
+    w.close()
+    os.close(w1)
+    r1 = os.fdopen(r1)
+    for i in range(total_lines):
+        linea = r1.read()
         print("%s" % linea)
         os.wait()
+    r1.close()
     file1.close()   
 
 if __name__ == '__main__':
